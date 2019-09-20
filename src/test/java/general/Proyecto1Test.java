c8b47d3d7b6c770cec9a476c45028f4fbbd5c4b9
kidiwnjt <kidiwnjt@kidiwn-pc>
2019-09-19 22:22:33 -0600

    Inicio

---
+++ src/test/java/general/Proyecto1Test.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package general;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+
+/**
+ *
+ * @author kidiwnjt
+ */
+public class Proyecto1Test {
+    
+    public Proyecto1Test() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+    
+}
